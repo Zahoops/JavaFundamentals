ac449fb2cf52d65cee6f571476213fbe709be8aa
Zahoops <zahoor_hussain95@hotmail.co.uk>
2024-06-15 23:28:23 -0500

    If Statements
    //Objective
    -Understanding if statements
    -Minor problem solving with if statements

---
+++ HelloWorld/src/ifProblemSolve.java
@@ -0,0 +1,17 @@
+public class ifProblemSolve {
+    public static void main (String [] args){
+//Code Block 1
+        int newValue = 50;
+
+        if (newValue == 50) {
+            System.out.println("This is an error");
+        }
+
+//Code Block 2
+        boolean isCar = true;
+
+        if (isCar == true){
+            System.out.println("This is not supposed to happen");
+        }
+    }
+}
